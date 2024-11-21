effcde0967f4c034974d124d1a3b8b2b08517f08
mpan <Matti.Panula@seamk.fi>
2024-11-21 11:00:41 +0200

    refactored drawing

--- printer.py
+++ printer.py
@@ -1,30 +1,7 @@
 import pygame
 import sys
 
-class rectangle:
-    x = 0
-    y = 0
-    width = 100
-    height = 100
-    color = (255, 0, 0)
-
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
-
-    def move_on(self, rect, x_fraction, y_fraction):
-        self.x = rect.x + x_fraction * rect.width
-        self.y = rect.y + y_fraction * rect.height
-
-    def get_fraction(self, rect):
-        return (self.x - rect.x) / rect.width, (self.y - rect.y) / rect.height
-
+from printer_ui import printer_ui
 
 # Initialize Pygame
 pygame.init()
@@ -34,18 +11,10 @@ width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Moving Box Animation")
 
-PADDING = 20
-
-plate_top = rectangle(PADDING, PADDING, 150, 200, (100, 100, 100))
-print_head_top = rectangle(40, 40, 10, 10, (255, 0, 0))
-plate_side = rectangle(PADDING + plate_top.width + PADDING, 100, 150, 120, (100, 100, 100))
-print_head_side = rectangle(300, 50, 10, 10, (255, 0, 0))
-
-print_head_top.move_on(plate_top, 0, 0)
-print_head_side.move_on(plate_side, 0, 0)
-
+printer_view = printer_ui()
 x = 0
 y = 0
+z = 0
 go_right = True
 
 # Main game loop
@@ -56,10 +25,7 @@ while running:
             running = False
 
     # Fill the screen with black
-    screen.fill((0,0,0))
-
-    print_head_top.move_on(plate_top, x, y)
-    print_head_side.move_on(plate_side, x, y)
+    screen.fill((0,0,0))    
 
     if x > 1:
         go_right = False
@@ -69,15 +35,15 @@ while running:
     if go_right:
         x += 0.01
         y += 0.01
+        z += 0.01
     else:
         x -= 0.01
         y -= 0.01
+        z -= 0.01
+    printer_view.move_to(x, y, z)
 
     # Draw the box
-    plate_top.draw(screen)
-    print_head_top.draw(screen)
-    plate_side.draw(screen)
-    print_head_side.draw(screen)
+    printer_view.draw(screen)
 
     # Update the display
     pygame.display.flip()
---
+++ printer_ui.py
@@ -0,0 +1,46 @@
+import pygame
+
+PADDING = 20
+
+class rectangle: # simple rectangle
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+    
+class grey_red_rectangle: # grey rectangle with movable small red rectangle on top
+    color_grey = (100, 100, 100)
+    color_red = (255, 0, 0)
+    red_size = 10
+
+    def __init__(self, x, y, width, height):
+        self.grey = rectangle(x, y, width, height, self.color_grey)
+        self.red = rectangle(0, 0, self.red_size, self.red_size, self.color_red)
+
+    def move_to(self, x_fraction, y_fraction):
+        self.red.x = self.grey.x + x_fraction * (self.grey.width - self.red_size)
+        self.red.y = self.grey.y + y_fraction * (self.grey.height - self.red_size)
+
+    def draw(self, screen):
+        self.grey.draw(screen)
+        self.red.draw(screen)
+
+class printer_ui: # two rectangles, one for top view, one for side view drawn side to side
+    def __init__(self):
+        self.top_view = grey_red_rectangle(PADDING, PADDING, 150, 200)
+        self.side_view = grey_red_rectangle(PADDING + self.top_view.grey.width + PADDING, 100, 150, 120)
+
+    def move_to(self, x, y, z):
+        self.top_view.move_to(x, y)
+        self.side_view.move_to(x, z)
+
+    def draw(self, screen):
+        self.top_view.draw(screen)
+        self.side_view.draw(screen)
+        
+        
